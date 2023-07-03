19bea1da93da0dcef9fe314bf629e08053cd6288
AshDev0 <college.ashwani@gmail.com>
2023-07-03 16:15:20 +0530

    Monday Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,19 @@
 module.exports = {
   content: [
     './assets/*.liquid',
-    './config/*.json',
     './layout/*.liquid',
     './sections/*.liquid',
     './snippets/*.liquid',
-    './templates/*.liquid',
-    './templates/*.json',
+    './templates/*.liquid'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'compacta': ['Compacta', 'Assistant', 'sans-serif'],
+        'kepler': ['Kepler'],
+        'tungsten': ['Tungsten', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
